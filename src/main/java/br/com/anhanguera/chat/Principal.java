ff67e1c502300661a0104fb3353ee3f8778808c9
Diego Silva <diegosiuniube@gmail.com>
2018-09-11 10:08:45 -0300

    fixes

--- src/main/java/br/com/anhanguera/chat/Principal.java
+++ src/main/java/br/com/anhanguera/chat/Principal.java
@@ -1,17 +1,12 @@
 package br.com.anhanguera.chat;
 
+import static spark.Spark.get;
 import static spark.Spark.init;
 import static spark.Spark.staticFiles;
-import static spark.Spark.get;
 import static spark.Spark.webSocket;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
-import org.eclipse.jetty.websocket.api.Session;
 
 import com.google.gson.Gson;
 
